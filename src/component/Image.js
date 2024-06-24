eb38d9579d039c4d21611a9a49a728f9209861b8
kalanithibala053 <kalanithibala053@gmail.com>
2024-06-24 21:07:24 +0530

    updated

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
 const QueryImageComponent = () => {
@@ -8,27 +8,29 @@ const QueryImageComponent = () => {
     const imageUrl = path.replace('/image/https:/', '');
     const [adjustedHeight, setAdjustedHeight] = useState('800px');
     const [adjustedWidth,setAdjustedWidth]=useState('auto');
-    const handleImageLoad = (event) => {
-        const imageHeight = event.target.naturalHeight;
-        const imageWidth=event.target.naturalWidth;
-        if (imageHeight > 600 && imageWidth < 500) {
-            setAdjustedHeight('450px');
-            setAdjustedWidth('500px');
-        }else if(imageHeight > 600){
-            setAdjustedHeight('450px');
-        }
-
-        
-    };
-
+    useEffect(() => {
+        const image = new Image();
+        image.src = imageUrl;
+        image.onload = () => {
+            const { naturalHeight, naturalWidth } = image;
+            if (naturalHeight > 600 && naturalWidth < 500) {
+                setAdjustedHeight('450px');
+                setAdjustedWidth('500px');
+            } else if (naturalHeight > 600) {
+                setAdjustedHeight('450px');
+            }
+        };
+        // Clean up image object
+        return () => {
+            image.onload = null;
+        };
+    }, [imageUrl]);
     return (
         <img
-        src={`https://${imageUrl}`} 
+            src={imageUrl}
             alt="Query"
-            width={adjustedHeight}
-            height={adjustedWidth}
+            style={{ height: adjustedHeight, width: adjustedWidth }}
             className='items-center mt-10 mx-auto'
-            onLoad={handleImageLoad}
         />
     );
 };
