9d7ca81d0d5b279c460f76c9c0656d1b69d0772e
kalanithibala053 <kalanithibala053@gmail.com>
2024-06-24 20:44:12 +0530

    updated

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../App.css';
 
 const QueryImageComponent = () => {
     const location = useLocation();
-
-    const path = location.pathname+location.search;
-
+    const path = location.pathname +location.search;
     const imageUrl = path.replace('/image/https:/', '');
+    const [adjustedHeight, setAdjustedHeight] = useState('auto');
+    
+    const handleImageLoad = (event) => {
+        const imageHeight = event.target.naturalHeight;
+        if (imageHeight > 600) {
+            setAdjustedHeight('600px');
+        }
+        
+    };
 
     return (
-        <img src={`https://${imageUrl}`} alt="Query" width="700" className='items-center mt-10 mx-auto' />
+        <img
+        src={`https://${imageUrl}`} 
+            alt="Query"
+            width="700"
+            height={adjustedHeight}
+            className='items-center mt-10 mx-auto'
+            onLoad={handleImageLoad}
+        />
     );
 };
 
