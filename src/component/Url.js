a4ab94cb4636b15f32e76510d326db9520e339eb
kalanithibala053 <kalanithibala053@gmail.com>
2024-06-25 10:45:54 +0530

    updated

--- src/component/Url.js
+++ src/component/Url.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import '../App.css';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 function Url() {
   const [url, setUrl] = useState('');
 
