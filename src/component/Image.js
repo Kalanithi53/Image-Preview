41b9291242979b013ea8d01d37638cea9c299233
kalanithibala053 <kalanithibala053@gmail.com>
2024-06-27 12:28:40 +0530

    updated

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../App.css';
-import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css'
 import ReactImageMagnifier from 'simple-image-magnifier/react';
 import Zoom from "react-img-zoom-gdn";
 import InnerImageZoom from 'react-inner-image-zoom';
